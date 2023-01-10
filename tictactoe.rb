ba60fae22745b8ad322af5d947f0cfb77dc355ed
Ron Javellana <Ronapj1991@gmail.com>
2023-01-10 12:53:10 -0500

    add method to check if square is marked

--- tictactoe.rb
+++ tictactoe.rb
@@ -12,14 +12,25 @@ class Board
     -----|-----|-----
       #{@squares[6]}  |  #{@squares[7]}  |  #{@squares[8]}  " + "\n"
     
-  def put_mark(square, mark)
-    @squares[square] = mark
+  def put_mark(squareNum, mark)
+    @squares[squareNum] = mark
+  end
+
+  def is_square_marked?(squareNum)
+    @squares[squareNum].equal?(" ")
   end
 end
 
+#display the board
 test = Board.new()
 puts test.displayBoard
 
 # change the values on the board
 test.put_mark(1, "X")
 puts test.displayBoard
+puts test.is_square_marked?(0)
+
+
+#check for wins
+#what are wins 
+# 012, 048, 036, 147, 246, 258, 345, 678
